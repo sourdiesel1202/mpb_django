1b06dd7b4401c0f838bef82c377b1c8df94a8716
Andrew <andrew@Andrews-MBP.lan>
2023-06-30 12:16:13 -0400

    changes for elastic beanstalk

--- terp_django/wsgi.py
+++ terp_django/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+0import os
 
 from django.core.wsgi import get_wsgi_application
 
