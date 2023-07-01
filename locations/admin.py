ea900d4e7c14224e21d9ef57962bb492a6fac6ae
Andrew <andrew@Andrews-MBP.lan>
2023-07-01 13:59:47 -0400

    Fix for locations?

--- locations/admin.py
+++ locations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from locations.models import *
+admin.site.register(Location)
 admin.site.register(LocationCity)
 admin.site.register(LocationCountry)
 admin.site.register(LocationState)
-admin.site.register(Location)
+
